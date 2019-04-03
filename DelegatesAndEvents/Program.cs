ef7233fdf01d1c411efb5397ac958faa5ad17694
kozhinamasha <kozhinamasha@gmail.com>
2019-04-03 12:51:05 +0300

    Delegate with using Lambda expression, Func<string, string> delegates, Action<string> delegates

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Net.Mime;
+using DelegatesAndEvents.SimpleDelegate;
 
 namespace DelegatesAndEvents
 {
     class Program
     {
+        delegate string MyNew(string text);
         static void Main(string[] args)
         {
+            //with additional arguments
             var video = new Video() { Title = "Video1" };
             var videoEncoder = new VideoEncoder(); //publisher
             var mailService = new MailService();     // subscriber
@@ -14,11 +18,38 @@ namespace DelegatesAndEvents
             videoEncoder.VideoEncoded += messageService.SendMessage;  // no() because it is a pointer to the method
             videoEncoder.Encode(video);
 
+            //without additional arguments
             User user = new User() { Name = "Tom" };
             LoginToPostman login = new LoginToPostman();
             OpenGithubPage page = new OpenGithubPage();
             login.LoggedIn += page.OpenPage;
             login.Login(user);
+
+            //the simple delegate C# 1.0
+            Printer print = SimpleDelegate.SimpleDelegate.PrintText;
+            print("simple delegate works correctly");
+
+            //delegate with using Lambda expression  C# 3.0
+            PrintWithLambdaSimple print3 = text => Console.WriteLine(text);
+            print3("more simple delegate with lambda");
+
+            MyNew mynew = text => text;
+            Console.WriteLine(mynew("My"));
+
+            //without declaration of delegate at all C# 3.5
+            Func<string, string> funcTest = delegate (string text)
+            {
+                return text;
+            };
+            Console.WriteLine(funcTest("Func delegates without delegate declaration"));
+
+            //without declaration of delegate with lambda C# 3.5
+            Func<string, string> funcTest2 = text => { return text; };
+            Console.WriteLine(funcTest2("The new func delegates with lambda"));
+
+            //action delegates
+            Action<string> actionTest = text => { Console.WriteLine(text); };
+            actionTest("Action delegates");
 
             Console.ReadKey();
         }
---
+++ DelegatesAndEvents/SimpleDelegate/SimpleDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DelegatesAndEvents.SimpleDelegate
+{
+    delegate void Printer(string text);
+    delegate void PrintWithLambdaSimple(string text);
+
+    public class SimpleDelegate
+    {
+        public static void PrintText(string text)
+        {
+            Console.WriteLine("The printed text is: " + text);
+        }
+    }
+}
